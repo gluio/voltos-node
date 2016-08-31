815795ff6ea4b02f3e3d937df08196a91cc06a9e
Glenn Gillen <me@glenngillen.com>
2016-08-31 14:09:46 +1000

    Extract common options.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request')
 var API_URL = 'https://voltos.io/v1'
+const apiRequestOpts = { strictSSL: false, json: true }
 
 var api = {
   signup: function(email, password, tokenName, callback) {
@@ -8,12 +9,12 @@ var api = {
       email: email,
       name: tokenName }
     if (password) { data.password = password }
-    request.post({ url: url, strictSSL: true, json: true, form: data }, callback)
+    request.post(Object.assign({}, apiRequestOpts, { url: url, form: data }), callback)
   },
   // POST /account/token
   userToken: function(email, password, callback) {
     var url = API_URL + '/account/token'
-    request.post({ url: url, strictSSL: true, json: true, auth: basicAuth(email, password) }, callback)
+    request.post(Object.assign({}, { url: url, auth: basicAuth(email, password) }), callback)
   },
   // POST /bundles
   create: function(name, tokenName, token, callback) {
@@ -21,12 +22,12 @@ var api = {
     var data = {
       name: name,
       token_name: tokenName }
-    request.post({ url: url, strictSSL: true, json: true, form: data, auth: tokenAuth(token) }, callback)
+    request.post(Object.assign({}, { url: url, form: data, auth: tokenAuth(token) }), callback)
   },
   // DELETE /bundles
   destroy: function(name, token, callback) {
     var url = API_URL + '/bundles/' + name
-    request.delete({ url: url, strictSSL: true, json: true, auth: tokenAuth(token) }, callback)
+    request.delete(Object.assign({}, { url: url, auth: tokenAuth(token) }), callback)
   },
   // POST /bundles/:name/token
   bundleToken: function(name, tokenName, token, callback) {
@@ -34,7 +35,7 @@ var api = {
     var data = {
       name: tokenName
     }
-    request.post({ url: url, strictSSL: true, json: true, form: data, auth: tokenAuth(token) }, callback)
+    request.post(Object.assign({}, { url: url, form: data, auth: tokenAuth(token) }), callback)
   },
   // PUT /bundles/:name
   rename: function(src, dest, token, callback) {
@@ -42,17 +43,17 @@ var api = {
     var data = {
       new_bundle_name: dest
     }
-    request.put({ url: url, strictSSL: true, json: true, form: data, auth: tokenAuth(token) }, callback)
+    request.put(Object.assign({}, { url: url, form: data, auth: tokenAuth(token) }), callback)
   },
   // GET /bundles/:list || GET /credentials
   list: function(name, token, callback) {
     var url = API_URL + '/credentials/' + name
-    request.get({ url: url, strictSSL: true, json: true, auth: tokenAuth(token) }, callback)
+    request.get(Object.assign({}, { url: url, auth: tokenAuth(token) }), callback)
   },
   // POST /credentials
   set: function(name, token, credentials, callback) {
     var url = API_URL + '/bundles/' + name
-    request.put({ url: url, strictSSL: true, json: true, form: credentials, auth: this.tokenAuth(token) }, callback)
+    request.put(Object.assign({}, { url: url, form: credentials, auth: this.tokenAuth(token) }), callback)
   },
   // DELETE /credentials
   unset: function(name, token, keys, callback) {
@@ -61,7 +62,7 @@ var api = {
     for (var i = 0, len = keys.length; i < len; i++) {
       data[keys[i]] = null
     }
-    request.delete({ url: url, strictSSL: true, json: true, form: data, auth: tokenAuth(token) }, callback)
+    request.delete(Object.assign({}, { url: url, form: data, auth: tokenAuth(token) }), callback)
   },
   // POST /bundles/:name/fork
   fork: function(src, dest, tokenName, token, callback) {
@@ -70,7 +71,7 @@ var api = {
       name: dest,
       token_name: tokenName
     }
-    request.post({ url: url, strictSSL: true, json: true, form: data, auth: tokenAuth(token) }, callback)
+    request.post(Object.assign({}, { url: url, form: data, auth: tokenAuth(token) }), callback)
   },
   // POST /bundles/:name/share
   share: function(name, emails, token, callback) {
@@ -78,7 +79,7 @@ var api = {
     var data = {
       email: emails
     }
-    request.post({ url: url, strictSSL: true, json: true, form: data, auth: tokenAuth(token) }, callback)
+    request.post(Object.assign({}, { url: url, form: data, auth: tokenAuth(token) }), callback)
   },
   // DELETE /bundles/:name/share
   retract: function(name, email, token, callback) {
@@ -86,7 +87,7 @@ var api = {
     var data = {
       email: email
     }
-    request.delete({ url: url, strictSSL: true, json: true, form: data, auth: tokenAuth(token) }, callback)
+    request.delete(Object.assign({}, { url: url, form: data, auth: tokenAuth(token) }), callback)
   },
   basicAuth: function(user, pass) {
     return { user: user, pass: pass}
